50ebde3aa5ed3c917108e1bf933905dfff6cf63e
Uyi <ihazauyi@gmail.com>
2022-04-28 22:53:43 +0100

    chore: Handle async function (try/catch)

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser  = require('body-parser');
 const expect      = require('chai').expect;
 const cors        = require('cors');
 require('dotenv').config();
+require('express-async-errors');
 
 const apiRoutes         = require('./routes/api.js');
 const fccTestingRoutes  = require('./routes/fcctesting.js');
