5ed2e0d90914f061576b8fa8dac3bb4c16af2753
Uyi <ihazauyi@gmail.com>
2022-04-29 11:26:28 +0100

    fix: Finally add project_id as the project on each issuse instead of the entire project object

--- controllers/api.js
+++ controllers/api.js
@@ -4,7 +4,7 @@ const Issue = require('../models/Issue.js');
 /* Get all issues concerning a project (with filters) */
 const getIssues = async function(req, res) {
   const project = await Project.find({name: req.params.project});
-  const issues = await Issue.find({projectId: project, ...req.query});
+  const issues = await Issue.find({projectId: project._id, ...req.query});
   //res.json({action: "get all issues", project, filters: req.query});
   res.json(issues);
 }
@@ -15,7 +15,7 @@ const createIssue = async function(req, res) {
   if (!project) {
     project = await Project.create({name: projectName});
   }
-  const issue = await Issue.create({projectId: project, ...req.body});
+  const issue = await Issue.create({projectId: project._id, ...req.body});
   res.status(201).json(issue);
 }
 
@@ -24,7 +24,7 @@ const updateIssue = async function(req, res) {
   const {_id} = req.body;
   let project = await Project.findOne({name: req.params.project});
   const issue = await Issue.findOneAndUpdate(
-    {_id, projectId: project},
+    {_id, projectId: project._id},
     req.body,
     {new: true, runValidators: true}
   );
@@ -35,7 +35,7 @@ const deleteIssue = async function(req, res) {
   const {_id} = req.body;
   let project = await Project.findOne({name: req.params.project});
   const issue = await Issue.findByIdAndRemove({
-    _id, projectId: project
+    _id, projectId: project._id
   });
   res.status(200).json({"action": "delete issue", issue});
 }
