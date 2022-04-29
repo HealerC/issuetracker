7608fe24a1f943895b649f93a5d41986a686c779
Uyi <ihazauyi@gmail.com>
2022-04-29 03:11:56 +0100

    refactor: Refactor roughly/simply to make it more readable to me

--- controllers/api.js
+++ controllers/api.js
@@ -3,32 +3,28 @@ const Issue = require('../models/Issue.js');
 
 /* Get all issues concerning a project (with filters) */
 const getIssues = async function(req, res) {
-  let project = req.params.project;
-  const projectId = await Project.find({name: project});
-  //const { _id, issue_title, issue_text, created_by, assigned_to, 
-  //        open, status, status_text, created_on, updated_on,   } = req.query;
-  const issues = await Issue.find({projectId, ...req.query});
+  const project = await Project.find({name: req.params.project});
+  const issues = await Issue.find({projectId: project, ...req.query});
   //res.json({action: "get all issues", project, filters: req.query});
   res.json(issues);
 }
 
 const createIssue = async function(req, res) {
-  let project = req.params.project;
-  let projectId = await Project.findOne({name: project});
-  if (!projectId) {
-    projectId = await Project.create({name: project});
-    projectId = projectId._id;
+  const projectName = req.params.project;
+  let project = await Project.findOne({name: projectName});
+  if (!project) {
+    project = await Project.create({name: projectName});
   }
-  const issue = await Issue.create({projectId, ...req.body});
+  const issue = await Issue.create({projectId: project, ...req.body});
   res.status(201).json(issue);
 }
 
 const updateIssue = async function(req, res) {
   //let project = req.params.project;
-  const {params: {project}, body: {_id}} = req;
-  let projectId = await Project.findOne({name: project});
+  const {_id} = req.body;
+  let project = await Project.findOne({name: req.params.project});
   const issue = await Issue.findOneAndUpdate(
-    {_id, projectId},
+    {_id, projectId: project},
     req.body,
     {new: true, runValidators: true}
   );
@@ -36,8 +32,8 @@ const updateIssue = async function(req, res) {
 }
 
 const deleteIssue = async function(req, res) {
-  let project = await Project.findOne({name: req.params.project});
   const {_id} = req.body;
+  let project = await Project.findOne({name: req.params.project});
   const issue = await Issue.findByIdAndRemove({
     _id, projectId: project
   });
