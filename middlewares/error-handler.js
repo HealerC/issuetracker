974b58ffee487386655835dc6d7fa971df03c53b
Uyi <ihazauyi@gmail.com>
2022-04-29 11:55:44 +0100

    feat(fcc): Provide error when user submits request with missing fields

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,4 +1,12 @@
 const errorHandler = (err, req, res, next) => {
-  return res.send(err || err.message);
+  const customError = {
+    statusCode: err.statusCode || 500,
+    message: err.message || 'Something went wrong, try again later'
+  }
+  if (err.name === 'ValidationError') {
+    customError.statusCode = 400;
+    customError.message = { error: "required field(s) missing" }
+  }
+  return res.status(customError.statusCode).json(customError.message);
 }
 module.exports = errorHandler;
