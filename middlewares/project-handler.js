f0c4104ffcff0297e9a1f3fe9ebc38d1b592b842
Uyi <ihazauyi@gmail.com>
2022-05-01 17:53:27 +0100

    feat: Handle user trying to access a project that does not exist

--- middlewares/project-handler.js
+++ middlewares/project-handler.js
@@ -1,7 +1,11 @@
 const Project = require('../models/Project.js');
+const {NotFoundError} = require('../errors');
 
 const getProject = async function (req, res, next) {
   const project = await Project.findOne({name: req.params.project});
+  if (!project) {
+    throw new NotFoundError(`The project "${req.params.project}" does not exist`);
+  }
   req.project = project;
   next();
 }
